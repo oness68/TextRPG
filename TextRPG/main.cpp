181b253c779532774a2e031ac40d10b8b5e73ef6
Malnal <rbgur211@naver.com>
2025-01-12 23:05:15 +0900

    [기능] Log 클래스 기본 설게 추가 기능 : 시작메뉴 출력, 게임 오버화면과 게임 종료화면 출력
    main함수 임시 변경 : Log클래스를 이용해 제어하도록 변경, 필요한 bool 변수 추가
    
    .gitignore : 기존 C++에서 VisualStudio ignore로 바꿔줌
    (https://github.com/github/gitignore/blob/main/VisualStudio.gitignore)

--- TextRPG/main.cpp
+++ TextRPG/main.cpp
@@ -1,40 +1,52 @@
 #include <iostream>
-
 #include "GameManager.h"
+#include "EnumCollection.h"
+#include "Log.h"
 
 using namespace std;
 
-void main() 
+void main()
 {
 	using GM = GameManger::GameManger;
 
 	GM& gameManager = GM::GetInstance();
 
-	// 배틀 매니저, 케릭터 생성
+	// 배틀 매니저, 케릭터 생성, 로그 싱글턴 인스턴스 생성
 	gameManager.GenerateBattleManager();
 	Character player1 = gameManager.GenerateCharacter();
+	Log* logger = Log::GetInstance();
 
+	bool isGameRun = false;		//게임의 실행여부
+	bool isGameStart = false;	//게임메뉴 탈출조건 위함
 	int menuOption;
 
 	// 1. 게임시작, 2. 종료
-	while (true) 
+	while (!isGameStart)
 	{
-		cout << "게임 메뉴" << endl;
-		cout << "1. 게임시작" << endl;
-		cout << "2. 게임종료" << endl;
+		logger->PrintStartMenu();
 		cin >> menuOption;
 
 		switch (menuOption) {
 		case 1:
+			isGameStart = true;	//시작화면 루프 벗어나기 위함
+			isGameRun = true;	//게임 시작
 			break;
 		case 2:
-			cout << "게임을 종료합니다..." << endl;
+			logger->PrintGameOver(LogEnum::GameEnd);	//플레이어의 사망과 게임 재시작 여부 물어보는 출력문
 			return;
 			break;
 		default:
-			cout << "잘못된 입력입니다. 다시 시도하세요." << endl;
+			logger->PrintInputError();	//잘못된 입력 안내 출력
 		}
 	}
 
+	//게임 실행 코드
+	while (isGameStart)
+	{
+		break;
+	}
+
+	//게임 실행이 끝났을 때
+	logger->PrintGameOver(LogEnum::GameEnd);
 	return;
 }
