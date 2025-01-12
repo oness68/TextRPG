ba5f48c08afd16f4218536a81d1a149347c7fc35
황찬호 <cksgh0316@gmail.com>
2025-01-12 13:15:08 +0900

    [황찬호][신규] 방 랜덤 선택

--- TextRPG/GameManager.cpp
+++ TextRPG/GameManager.cpp
@@ -1,5 +1,7 @@
 ﻿#include "GameManager.h"
-//#include "Character.h"
+
+#include <iostream>
+using namespace std;
 
 namespace GameManger {
 
@@ -7,6 +9,7 @@ namespace GameManger {
     GameManger& GameManger::GetInstance() 
     {
         static GameManger instance;
+
         return instance;
     }
 
@@ -64,5 +67,56 @@ namespace GameManger {
     void GameManger::BeginPlay()
     {
         // TODO: 구현 필요
+        SetStage(1);
+
+        std::srand(static_cast<unsigned>(std::time(nullptr)));
+        std::vector<StageRooms> selectedRooms = GenerateTwoRandomRooms(roomProbabilities);
+
+        cout << "Room: " << StageRoomToString(selectedRooms[0]) << endl;
+        cout << "Room: " << StageRoomToString(selectedRooms[1]) << endl;
     }
+
+    StageRooms GameManger::GenerateRandomRoom(const std::map<StageRooms, double>& roomProbabilities)
+    {
+        double totalProbability = 0.0;
+
+        for (const auto& room : roomProbabilities) 
+        {
+            totalProbability += room.second;
+        }
+
+        double randomValue = (std::rand() % 10000) / 10000.0 * totalProbability; // std::rand()는 0부터 32767까지의 난수를 생성 0 ~ 9999 나머지
+        double cumulativeProbability = 0.0;
+
+        for (const auto& room : roomProbabilities) 
+        {
+            cumulativeProbability += room.second;
+            if (randomValue <= cumulativeProbability) 
+            {
+                return room.first;
+            }
+        }
+
+        return Shop;
+    }
+
+    // 배틀방만 중복이 가능하도록 처리한 랜덤 방 선택 함수
+    std::vector<StageRooms> GameManger::GenerateTwoRandomRooms(const std::map<StageRooms, double>& roomProbabilities) 
+    {
+        std::vector<StageRooms> selectedRooms;
+
+        StageRooms firstRoom = GenerateRandomRoom(roomProbabilities);
+        selectedRooms.push_back(firstRoom);
+
+        StageRooms secondRoom;
+        do 
+        {
+            secondRoom = GenerateRandomRoom(roomProbabilities);
+        } while (firstRoom != Battle && secondRoom == firstRoom);
+
+        selectedRooms.push_back(secondRoom);
+
+        return selectedRooms;
+    }
+
 } // namespace GameManger
--- TextRPG/main.cpp
+++ TextRPG/main.cpp
@@ -5,7 +5,7 @@
 
 using namespace std;
 
-void main() 
+int main() 
 {
 	using GM = GameManger::GameManger;
 
@@ -13,8 +13,8 @@ void main()
 
 	// 배틀 매니저, 케릭터 생성
 	gameManager.GenerateBattleManager();
-	Character player1 = *new Character("한정혁");
-	player1.DisplayStatus();
+	//Character player1 = *new Character("한정혁");
+	//player1.DisplayStatus();
 
 	int menuOption;
 
@@ -28,15 +28,16 @@ void main()
 
 		switch (menuOption) {
 		case 1:
+			gameManager.BeginPlay();
 			break;
 		case 2:
 			cout << "게임을 종료합니다..." << endl;
-			return;
+			return 0;
 			break;
 		default:
 			cout << "잘못된 입력입니다. 다시 시도하세요." << endl;
 		}
 	}
 
-	return;
+	return 0;
 }
