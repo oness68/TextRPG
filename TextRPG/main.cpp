b06a0391457d451bb1215e4e7b3b385c32b58115
Malnal <rbgur211@naver.com>
2025-01-14 21:34:29 +0900

    [채규혁][수정]
    브랜치 맛이 가서 그냥 메인에서부터 새로 팠습니다.
    
    PrintLog 오버로딩으로 구현해서 문자열만 출력하는 경우는
    PrintLog("문자열") 형태로 하시면 됩니다.
    추가로 배틀에서 이미지 두개 출력을 위해
    PrintLog(string orderLog, int caseNumber1, caseNumber2) 구현 예정입니다.

--- TextRPG/main.cpp
+++ TextRPG/main.cpp
@@ -16,28 +16,28 @@ void main()
 	// 배틀 매니저, 케릭터 생성
 	gameManager.GenerateBattleManager();
 	Log* logger = Log::GetInstance();
+	logger->Initialize();
 	Character player1 = *new Character("한정혁");
-	player1.DisplayStatus();
+	//player1.DisplayStatus();
 
 	int menuOption;
 	bool isGameRun = false;		//게임의 실행여부
 	bool isGameStart = false;	//게임메뉴 탈출조건 위함
 
-	logger->PrintLog("Hello", 0);
 	// 1. 게임시작, 2. 종료
 	while (!isGameStart)
 	{
-		logger->PrintStartMenu(Menu);
+		logger->PrintStartMenu(EMenu);
 		cin >> menuOption;
 
 		switch (menuOption) {
 		case 1:
 			isGameStart = true;	//시작화면 루프 벗어나기 위함
 			isGameRun = true;	//게임 시작
-			logger->PrintStartMenu(Start);
+			logger->PrintStartMenu(EStart);
 			break;
 		case 2:
-			logger->PrintGameOver(LogEnum::GameEnd);	//플레이어의 사망과 게임 재시작 여부 물어보는 출력문
+			logger->PrintGameOver(EGameEnd);	//플레이어의 사망과 게임 재시작 여부 물어보는 출력문
 			return;
 			break;
 		default:
@@ -48,10 +48,12 @@ void main()
 	//게임 실행 코드
 	while (isGameStart)
 	{
+		//테스트로 한 번 Log 출력 해보겠습니다.
+		logger->PrintLog("테스트 출력입니다~", EDragon);
 		break;
 	}
 
 	//게임 실행이 끝났을 때
-	logger->PrintGameOver(LogEnum::GameEnd);
+	logger->PrintGameOver(EGameEnd);
 	return;
 }
